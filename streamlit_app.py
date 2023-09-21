bab66ea92cb79a1b64034e72240ab5e0ce6a6314
negi1221 <144980920+negi1221@users.noreply.github.com>
2023-09-21 23:56:45 +0000

    ｓｇｓｇ

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,27 @@ import random
 # タイトルを設定
 st.title('おみくじアプリをつくってみたよ！のアプリ')
 
+user_name = st.text_input("あなたの名前を入力してください。")
+
 if st.button("おみくじを引く"):
-    results = ["大吉","大吉","大吉","大吉","大吉","中吉","小吉","吉","凶","大凶"]
+    results = ["大吉","中吉","小吉","吉","凶","大凶"]
     result = random.choice(results)
 
-    #結果を表示する
-    st.write(f"結果:{result}")
+    comments = {
+        "大吉": "素晴らしい一日になるでしょう❣",
+        "中吉": "良いことがあるかも！？"
+        "小吉": "悪くない気がしなくもなくもない"
+        "吉": "soso"
+        "凶": "注意が大事"
+        "大凶": "おみくじを信じないのも一つの手です"
+    }
 
-#ハッシュタグはコメントを書くことができるよ！
+    #結果を表示する
+    if user_name:
+        st.write(f"{user_name}さんの結果: {result}")
+    else:
+        st.write(f"結果: {result}")
+    
+    st.write(comments[result])
+
+    
