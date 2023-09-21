913ee217fc813b6174e0e0b63a7a2ae740b40e98
negi1221 <144980920+negi1221@users.noreply.github.com>
2023-09-21 04:37:36 +0000

    ｓｇ

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ import random
 st.title('おみくじアプリをつくってみたよ！のアプリ')
 
 if st.button("おみくじを引く"):
-    results = ["大吉","中吉","小吉","吉","凶","大凶"]
+    results = ["大吉","大吉","大吉","大吉","中吉","小吉","吉","凶","大凶"]
     result = random.choice(results)
     st.write(f"結果:{result}")
